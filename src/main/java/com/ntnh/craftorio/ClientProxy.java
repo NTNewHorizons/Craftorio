da608deed1306e9e059af6bfb610dce194029a40
Bufka Second <4129261@school.ecsd.net>
2026-04-07 17:21:04 +0000

    We are not MyMod

--- src/main/java/com/ntnh/craftorio/ClientProxy.java
+++ src/main/java/com/ntnh/craftorio/ClientProxy.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.ntnh.craftorio;
 
 public class ClientProxy extends CommonProxy {
 
--- src/main/java/com/ntnh/craftorio/Config.java
+++ src/main/java/com/ntnh/craftorio/Config.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.ntnh.craftorio;
 
 import java.io.File;
 
